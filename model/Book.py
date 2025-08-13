79f5e343aa60cd92225962738e94e914555d8bcf
HalaMadi <Halamadi75@gmail.com>
2025-08-13 14:06:01 +0300

    Implement Reservable functionality and reserve method

--- model/Book.py
+++ model/Book.py
@@ -1,13 +1,26 @@
+from Reservable import Reservable
 from model.LibraryItem import LibraryItem
-class Book(LibraryItem):
+class Book(LibraryItem,Reservable):
     def __init__(self, title, author, book_id, available=True):
         super().__init__(title, author)
         self.book_id = book_id
         self.available = available
+        self.reserved_by = None
 
     def display_info(self):
         super().display_info()
         status = "Available" if self.available else "Not Available"
         print(f"Book ID: {self.book_id} | Status: {status}")
+        
     def check_availability(self):
         return self.available
+
+    def reserve(self,user):
+        try:
+            if self.reserved_by is None:
+                self.reserved_by=user
+                print(f"{self.title} reserved by {user}.")
+            else:
+                raise Exception("This book is already reserved.")
+        except Exception as e:
+            print(f"Error reserving book: {e}")
--- model/DVD.py
+++ model/DVD.py
@@ -1,15 +1,27 @@
+from Reservable import Reservable 
 from model.LibraryItem import LibraryItem
 
-class DVD(LibraryItem):
+class DVD(LibraryItem, Reservable):
     def __init__(self, title, author, dvd_id, available=True):
         super().__init__(title, author)
         self.dvd_id = dvd_id
         self.available = available
+        self.reserved_by = None
 
     def display_info(self):
         super().display_info()
-        print(f"DVD ID: {self.dvd_id}")
-        print(f"Availability: {'Available' if self.available else 'Not Available'}")
+        status="Available" if self.available else "Not Available"
+        print(f"DVD ID: {self.dvd_id} | Status: {status}")
 
     def check_availability(self):
         return self.available
+    
+    def reserve(self,user):
+        try:
+            if self.reserved_by is None:
+                self.reserved_by=user
+                print(f"{self.title} reserved by {user}.")
+            else:
+                raise Exception("This DVD is already reserved.")
+        except Exception as e:
+            print(f"Error reserving DVD: {e}")
